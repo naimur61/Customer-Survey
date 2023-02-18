b6ad5c0bef72c269036bc4535965cd83387bcf86
Naimur <naimur.r61n@gmail.com>
2023-02-18 13:53:22 +0600

    servey

--- src/Pages/Surveys.js
+++ src/Pages/Surveys.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Surveys = () => {
    return (
       <Stack direction='column' justifyContent='center' alignItems='center' height='80vh'>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facilis rerum reprehenderit voluptates dolorem qui quisquam eaque impedit illum, possimus dolores atque quis adipisci cum? Facere minima quas facilis corrupti!
+
       </Stack>
    );
 };
