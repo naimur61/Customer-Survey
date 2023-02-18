0708079cf52012863488f7466abb212b5b3a788e
Naimur <naimur.r61n@gmail.com>
2023-02-18 13:30:14 +0600

    home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,24 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { BsCheckCircleFill } from 'react-icons/bs';
+import EastIcon from '@mui/icons-material/East';
+
 
 const Home = () => {
    return (
-      <Stack direction='column' justifyContent='center' alignItems='center' height='50vh'>
+      <Stack direction='column' justifyContent='center' alignItems='center' height='80vh'>
          <Typography variant='h3' sx={{
             fontWeight: 600
          }}>
             Take a Surveys.
             <BsCheckCircleFill style={{ color: "#0d6efd", marginLeft: '1rem', marginBottom: "-10px" }} />
          </Typography>
-         <Typography variant='h5' color='GrayText' fontWeight='600' mt='5px'>
+         <Typography variant='h5' color='GrayText' fontWeight='600' mt='1rem' mb='7rem'>
             It will take 5 mounties!
          </Typography>
-
+         <Button variant="contained" endIcon={<EastIcon />} >
+            Start
+         </Button>
       </Stack>
    );
 };
